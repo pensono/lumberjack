78723dd74a4e60a95d56369f47a5d24a979ea8d4
Ethan Shea <ethan.shea1@gmail.com>
2022-11-20 22:32:54 -0500

    Add github link

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,18 @@
   <v-app>
     <v-app-bar title="Lumberjack" :elevation="0" density="compact">
       <v-btn>
+        <a href="https://github.com/pensono/lumberjack" target="_blank" rel="noreferrer noopener" >
+          <v-icon icon="fa-brands fa-github" />
+        </a>
+      </v-btn>
+      <v-btn>
         Demo
         <v-menu activator="parent">
           <v-list>
             <v-list-item
-                v-for="demo in demos"
-                :key="demo.name"
-                @click="loadDemo(demo)"
+              v-for="demo in demos"
+              :key="demo.name"
+              @click="loadDemo(demo)"
             >
               <v-list-item-title>{{ demo.name }}</v-list-item-title>
             </v-list-item>
@@ -33,7 +38,9 @@
                 v-model="code"
                 placeholder="Queries go here..."
               />
-              <div v-if="errorMessage" class="error-box">{{ errorMessage }}</div>
+              <div v-if="errorMessage" class="error-box">
+                {{ errorMessage }}
+              </div>
             </pane>
           </splitpanes>
         </pane>
@@ -52,12 +59,12 @@
 <script setup lang="ts">
 import * as dfd from "danfojs";
 import "splitpanes/dist/splitpanes.css";
-import {computed, ref, watch} from "vue";
-import {evaluate} from "@/lib/evaluator";
-import {toDataFrame} from "@/lib/raw_input";
-import {SimpleContext} from "@/lib/types";
+import { computed, ref, watch } from "vue";
+import { evaluate } from "@/lib/evaluator";
+import { toDataFrame } from "@/lib/raw_input";
+import { SimpleContext } from "@/lib/types";
 import DataFrameView from "@/components/DataFrameView.vue";
-import {Demo, demos} from "@/demo_resources";
+import { Demo, demos } from "@/demo_resources";
 import axios from "axios";
 
 const code = ref("");
@@ -92,8 +99,7 @@ const errorMessage = computed<string | null>(() => {
 });
 
 function loadDemo(demo: Demo) {
-  axios.get(demo.log_url)
-      .then(result => {
+  axios.get(demo.log_url).then((result) => {
     dataRaw.value = result.data;
     code.value = demo.query;
   });
@@ -109,7 +115,6 @@ main {
   height: 100%;
 }
 
-
 .code-container {
   display: flex;
   flex-direction: column;
@@ -120,7 +125,7 @@ main {
 }
 
 .splitpanes__splitter {
-  padding: .25em;
+  padding: 0.25em;
 }
 
 .cm-editor {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import '@fortawesome/fontawesome-free/css/all.css'
 import { createApp } from 'vue'
 import VueCodemirror from 'vue-codemirror'
 import {createVuetify} from "vuetify"
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, fa } from 'vuetify/iconsets/fa'
 import 'vuetify/styles'
 import { Splitpanes, Pane } from 'splitpanes'
 import App from './App.vue'
@@ -10,7 +12,16 @@ import './assets/style.scss'
 
 createApp(App)
     .use(VueCodemirror)
-    .use(createVuetify({components, directives}))
+    .use(createVuetify({
+        components,
+        directives,
+        icons: {
+            defaultSet: 'fa',
+            sets: {
+                fa
+            }
+        }
+    }))
     .component("splitpanes", Splitpanes)
     .component("pane", Pane)
     .mount('#app')
